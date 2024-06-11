9fb6a8920b284aee1bc54e24b2fdd3fb719a46d3
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-06-11 21:04:00 +0200

    M0

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+<form action="./elabora.php" method="GET">
+    <input type="text" placeholder="name" name="name">
+    <button type="submit">invia</button>
+</form>
+</body>
+</html>
